34ebbde881a5896707efa725f91bc9bb17afb41c
lfilella97 <lfilella97@gmail.com>
2023-01-30 21:48:51 +0100

    add App Component and the css styles given

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+const App = (): JSX.Element => {
+  return (
+    <div className="container">
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+      <main className="main">
+        <ul className="gentlemen"></ul>
+      </main>
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,9 @@
+const Info = (): JSX.Element => {
+  return (
+    <section class="controls">
+      <p class="info">0 gentlemen pointing at you</p>
+      <button class="button button--select">Select all</button>
+    </section>
+  );
+};
+export default Info;
