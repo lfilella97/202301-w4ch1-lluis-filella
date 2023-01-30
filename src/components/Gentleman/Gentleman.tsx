2cdf49ac5e2fe7bdb934c80ff703beb1959a72de
lfilella97 <lfilella97@gmail.com>
2023-01-30 23:58:55 +0100

    add Info and Gentleman components

---
+++ src/components/Gentleman/Gentleman.tsx
@@ -0,0 +1,36 @@
+import GentlemanStructure from "./types";
+
+const Gentleman = (gentleman: GentlemanStructure): JSX.Element => {
+  return (
+    <>
+      <div className="gentleman__avatar-container">
+        <img
+          className="gentleman__avatar"
+          src={"img/" + gentleman.picture}
+          alt={gentleman.alternativeText}
+        />
+        <span className="gentleman__initial">{gentleman.name[1]}</span>
+      </div>
+      <div className="gentleman__data-container">
+        <h2 className="gentleman__name">{gentleman.name}</h2>
+        <ul className="gentleman__data-list">
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Profession:</span>
+            {gentleman.profession}
+          </li>
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Status: </span>
+            {gentleman.status}
+          </li>
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Twitter: </span>
+            {gentleman.twitter}
+          </li>
+        </ul>
+      </div>
+      <i className="icon gentleman__icon fas fa-check"></i>
+      <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+    </>
+  );
+};
+export default Gentleman;
---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,11 @@
+import Button from "../Button/Button";
+
+const Info = (): JSX.Element => {
+  return (
+    <section className="controls">
+      <p className="info">0 gentlemen pointing at you</p>
+      <Button />
+    </section>
+  );
+};
+export default Info;
