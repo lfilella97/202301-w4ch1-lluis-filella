a8d9d28140b03e33a6699b89c2c407b595311d3f
lfilella97 <lfilella97@gmail.com>
2023-01-30 21:48:51 +0100

    add App Component and the css styles given

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +9,11 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
