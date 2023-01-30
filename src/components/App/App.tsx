063f0c084a730eab19ce537fe4beb0fcb21b8d49
lfilella97 <lfilella97@gmail.com>
2023-01-31 00:01:21 +0100

    add images and intanciate Gentlemans on App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,21 @@
+import Gentleman from "../Gentleman/Gentleman";
+import Info from "../Info/Info";
+import { gentlemanList } from "./gentleManList";
 const App = (): JSX.Element => {
   return (
     <div className="container">
       <header className="main-header">
         <h1 className="main-title">The pointing gentlemen</h1>
       </header>
+      <Info />
       <main className="main">
-        <ul className="gentlemen"></ul>
+        <ul className="gentlemen">
+          {gentlemanList.map((gentleman, position) => (
+            <li className="gentleman" key={position}>
+              <Gentleman {...gentleman} />
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
